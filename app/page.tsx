7e066756b63ef80cf0f55577bb09e70d74bf172b
Thegreatsura <167956758+Thegreatsura@users.noreply.github.com>
2024-05-14 17:42:02 +0000

    transaction page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import LeftSide from "@/components/authentication/left-side";
-import { BentoDemo } from "@/components/magicui/test";
-import { AnimatedListDemo } from "@/components/magicui/test2";
-import { Hello } from "@/components/magicui/test3";
-import App from "@/components/magicui/test4";
-import RightSide from "@/components/right-side";
-import Image from "next/image";
+import TransactionPage from "@/components/transaction/transaction";
 
 export default function Home() {
-  return (
-    <main className="flex h-full items-center justify-center">
-      <div>hello</div>
-    </main>
-  );
+  return <TransactionPage />;
 }
